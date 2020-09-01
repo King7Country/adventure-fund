a3d9d03a2f4ca9a0cd9ad35689a1951e8be3b30f
C.J <cj@kingcountry.ca>
2020-09-01 07:58:11 -0700

    fixing build issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@
 
 // You can delete this file if you're not using it
 
-/************
+
 const path = require("path")
 
 exports.createPages = async ({ graphql, actions }) => {
@@ -20,78 +20,61 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-      allContentfulHeroBanner {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
     }
   `)
 
-  response.data.allContentfulBlogPost.edges.forEach(post => {
+  response.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
-      path: `/news/${post.node.slug}`,
-      component: path.resolve("./src/templates/blog-post.js"),
-      context: {
-        slug: post.node.slug,
-      },
-    })
-  })
-  response.data.allContentfulHeroBanner.edges.forEach(edge => {
-    createPage({
-      path: `/${edge.node.slug}`,
-      component: path.resolve("./src/templates/hero-post.js"),
+      path: `/news/${edge.node.slug}`,
+      component: path.resolve("./src/templates/news-article.js"),
       context: {
         slug: edge.node.slug,
       },
     })
   })
-  return
 }
-************/
 
 
 
-const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  return new Promise((resolve, reject) => {
-    const newsArticle = path.resolve('./src/templates/news-article.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
-              }
-            }
-          }
-          `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
-        }
-
-        const articles = result.data.allContentfulBlogPost.edges
-        articles.forEach((article, index) => {
-          createPage({
-            path: `/news/${article.node.slug}/`,
-            component: newsArticle,
-            context: {
-              slug: article.node.slug
-            },
-          })
-        })
-      })
-    )
-  })
-}
+// const path = require('path')
+//
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//
+//   return new Promise((resolve, reject) => {
+//     const newsArticle = path.resolve('./src/templates/news-article.js')
+//     resolve(
+//       graphql(
+//         `
+//           {
+//             allContentfulBlogPost {
+//               edges {
+//                 node {
+//                   title
+//                   slug
+//                 }
+//               }
+//             }
+//           }
+//           `
+//       ).then(result => {
+//         if (result.errors) {
+//           console.log(result.errors)
+//           reject(result.errors)
+//         }
+//
+//         const articles = result.data.allContentfulBlogPost.edges
+//         articles.forEach((article, index) => {
+//           createPage({
+//             path: `/news/${article.node.slug}/`,
+//             component: newsArticle,
+//             context: {
+//               slug: article.node.slug
+//             },
+//           })
+//         })
+//       })
+//     )
+//   })
+// }
