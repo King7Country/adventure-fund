cb45e940f16b9d95adc393493eff49c232b8baa6
C.J <cj@kingcountry.ca>
2020-09-23 13:48:25 -0700

    Configured google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,18 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Adventure Fund`,
-    description: `The world's top wealth and travel investment firm. It's never too late to invest in adventure.`,
+    description: `Site template built using React and Gatsby, content queried with GraphQL from Contentful CMS. Continuous deployment using Netlify and Git.`,
     author: `King Country Web Development`,
     keywords: `web development, web design, freelance, web developer, template, react, gatsby, contentful`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-162021154-2",
+        head: true,
+      },
+    },
     'gatsby-plugin-emotion',
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
@@ -66,37 +73,7 @@ module.exports = {
         icon: `src/images/kingcountry-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
 
-
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-162021154-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        //pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "adventurefund.netlify.app",
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
